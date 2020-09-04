0decbdf1db9aff7561ecf5ecf35ccffab417d3ef
t0xic0der <akashdeep.dhar@gmail.com>
2020-09-04 18:58:24 +0530

    Reworked client UI for themed terminals
    Version bump for client

--- cnew.py
+++ cnew.py
@@ -1,4 +1,4 @@
-import asyncio, websockets, time, json, click, secrets, os, sys, ast
+import asyncio, websockets, time, json, click, secrets, os, sys
 from prompt_toolkit import PromptSession
 from prompt_toolkit.patch_stdout import patch_stdout
 from prompt_toolkit import print_formatted_text, HTML
@@ -45,7 +45,7 @@ async def consumer_handler(cphrsuit, websocket, username, chatroom, servaddr):
 
 
 async def producer_handler(cphrsuit, websocket, username, chatroom, servaddr):
-    footelem = HTML("<b><style bg='seagreen'>" + username.strip() + "</style></b>@<b><style bg='seagreen'>" + chatroom + "</style></b> [<b><style bg='seagreen'>Sanctuary ZERO v19082020</style></b> running on <b><style bg='seagreen'>" + servaddr + "</style></b>]")
+    footelem = HTML("<b>[" + chatroom + "]</b>" + " " + username.strip() + " - Sanctuary ZERO v04092020 running on '" + servaddr + "' - Hit Ctrl+C to EXIT")
     while True:
         with patch_stdout():
             mesgtext = await sess.prompt_async("[" + obtntime() + "] " + formusnm(str(username)) + " ⮞ ", bottom_toolbar=footelem, validator=emtyfind())
@@ -70,14 +70,10 @@ def obtntime():
     timehour = str(timestmp.tm_hour)
     timemint = str(timestmp.tm_min)
     timesecs = str(timestmp.tm_sec)
-    if int(timehour) < 10:
-        timehour = "0" + timehour
-    if int(timemint) < 10:
-        timemint = "0" + timemint
-    if int(timesecs) < 10:
-        timesecs = "0" + timesecs
-    timestrg = timehour + ":" + timemint + ":" + timesecs
-    return timestrg
+    if int(timehour) < 10:  timehour = "0" + timehour
+    if int(timemint) < 10:  timemint = "0" + timemint
+    if int(timesecs) < 10:  timesecs = "0" + timesecs
+    return timehour + ":" + timemint + ":" + timesecs
 
 
 def randgene():
@@ -106,12 +102,9 @@ def chekpass(pswd):
 
 
 def formusnm(username):
-    if len(username) < 10:
-        return username + " " * (10 - len(username))
-    elif len(username) > 10:
-        return username[0:10]
-    else:
-        return username
+    if len(username) < 10:      return username + " " * (10 - len(username))
+    elif len(username) > 10:    return username[0:10]
+    else:                       return username
 
 
 @click.command()
@@ -119,44 +112,44 @@ def formusnm(username):
 @click.option("-p", "--password", "password", help="Enter the chatroom password for decrypting the messages")
 @click.option("-c", "--chatroom", "chatroom", help="Enter the chatroom identity you would want to enter in")
 @click.option("-s", "--servaddr", "servaddr", help="Enter the server address you would want to connect to", required=True)
-@click.version_option(version="19082020", prog_name="SNCTRYZERO Client by t0xic0der")
+@click.version_option(version="04092020", prog_name="SNCTRYZERO Client by t0xic0der")
 def mainfunc(username, password, chatroom, servaddr):
     try:
         os.system("clear")
-        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <b><seagreen>Starting Sanctuary ZERO v19082020 up...</seagreen></b>"))
-        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <lightgreen>Connected to " + servaddr + " successfully</lightgreen>"))
-        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <lightgreen>Session started at " + str(time.ctime()) + "</lightgreen>"))
+        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <b><seagreen>Starting Sanctuary ZERO v04092020 up...</seagreen></b>"))
+        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <seagreen>Connected to '" + servaddr + "' successfully</seagreen>"))
+        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <seagreen>Session started at " + str(time.ctime()) + "</seagreen>"))
         if chatroom is None:
-            print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <yellow>A new chatroom was generated</yellow>"))
+            print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <green>A new chatroom was generated</green>"))
             chatroom = randgene()
         else:
             if chekroom(chatroom) is True:
-                print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <yellow>A valid chatroom identity was entered</yellow>"))
+                print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <green>A valid chatroom identity was entered</green>"))
             else:
-                print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <yellow>An invalid chatroom identity was entered</yellow>"))
+                print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <green>An invalid chatroom identity was entered</green>"))
                 sys.exit()
         if password is None:
-            print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <yellow>A new password was generated</yellow>"))
+            print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <green>A new password was generated</green>"))
             password = Fernet.generate_key().decode("utf8")
         else:
             if chekpass(password) is True:
-                print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <yellow>A valid chatroom password was entered</yellow>"))
+                print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <green>A valid chatroom password was entered</green>"))
             else:
-                print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <yellow>An invalid chatroom password was entered</yellow>"))
+                print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <green>An invalid chatroom password was entered</green>"))
                 sys.exit()
-        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <cyan>Chatroom identity <white>⮞</white> " + chatroom + "</cyan>"))
-        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <cyan>Chatroom password <white>⮞</white> " + password + "</cyan>"))
-        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <lightgreen>Share the chatroom identity and password to add members!</lightgreen>"))
-        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <lightgreen>Your conversations are protected with end-to-end encryption</lightgreen>"))
+        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <seagreen>Chatroom identity : " + chatroom + "</seagreen>"))
+        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <seagreen>Chatroom password : " + password + "</seagreen>"))
+        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <seagreen>Share the chatroom identity and password to add members!</seagreen>"))
+        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <seagreen>Your conversations are protected with end-to-end encryption</seagreen>"))
         asyncio.get_event_loop().run_until_complete(hello(servaddr, username, chatroom, password))
     except KeyboardInterrupt as EXPT:
-        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <yellow>Leaving SNCTRYZERO...</yellow>"))
+        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <red>Leaving SNCTRYZERO...</red>"))
         sys.exit()
     except OSError as EXPT:
-        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <yellow>A connection to the server could not be established</yellow>"))
+        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <red>A connection to the server could not be established</red>"))
         sys.exit()
     except websockets.exceptions.ConnectionClosedError as EXPT:
-        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <yellow>A connection to the server was lost</yellow>"))
+        print_formatted_text(HTML("[" + obtntime() + "] " + "SNCTRYZERO ⮞ <red>A connection to the server was lost</red>"))
         sys.exit()
 
 
